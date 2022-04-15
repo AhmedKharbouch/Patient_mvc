c36c18460273188a7474439882e5e77ace61dec4
ahmed <Kharbouch_ahmed@hotmail.com>
2022-04-15 20:33:48 +0200

    First commit

---
+++ src/main/java/com/example/patient_mvc/PatientMvcApplication.java
@@ -0,0 +1,44 @@
+package com.example.patient_mvc;
+
+import com.example.patient_mvc.entities.Patient;
+import com.example.patient_mvc.repositories.PatientRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
+
+@SpringBootApplication
+public class PatientMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatientMvcApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(PatientRepository patientRepository){
+
+        return args -> {
+
+           /* patientRepository.save(new Patient(null,"ahmed",new Date(),false,12));
+            patientRepository.save(new Patient(null,"hassan",new Date(),true,50));
+            patientRepository.save(new Patient(null,"yassmine",new Date(),false,600));
+            patientRepository.save(new Patient(null,"houda",new Date(),true,25));
+
+            */
+            patientRepository.findAll().forEach(p->{
+
+                System.out.println(p.getNom());
+
+            });
+
+
+
+        };
+
+
+    }
+
+
+}
---
+++ src/test/java/com/example/patient_mvc/PatientMvcApplicationTests.java
@@ -0,0 +1,20 @@
+package com.example.patient_mvc;
+
+import com.example.patient_mvc.entities.Patient;
+import com.example.patient_mvc.repositories.PatientRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
+
+@SpringBootTest
+class PatientMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+
+}
